7777ceed4de73e866d23bb5e887767ac51e1ae62
MULTIJACKER <j.kemnitzer@web.de>
2021-05-27 00:05:39 +0200

    index.html and more modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>STONK-MANAGER</h1>
+      <p>Stonks0</p>
+      <p>Stonks1</p>
+      <p>Stonks2</p>
+      <p>Stonks3</p>
+      <p>.</p>
+      <p>.</p>
+      <p>.</p>
     </div>
   );
 }
